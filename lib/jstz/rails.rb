670a7b27fc9407c505117b8c67e5175c15f1e34f
Chip Duffield <chip.duffield@gmail.com>
2016-08-24 08:38:13 -0400

    Initial Commit

---
+++ lib/jstz/rails.rb
@@ -0,0 +1,6 @@
+module Jstz
+  module Rails
+    class Engine < ::Rails::Engine
+    end
+  end
+end
---
+++ test/jstz/rails_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Jstz::Rails::Test < ActiveSupport::TestCase
+  test "truth" do
+    assert_kind_of Module, Jstz::Rails
+  end
+end
